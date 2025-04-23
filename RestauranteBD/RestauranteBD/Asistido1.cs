6fc19263b53ccf9dcf7f6ddbcc49c47cc70430ee
SrAlejo4 <alejandrogarci087@gmail.com>
2025-04-22 23:34:02 -0500

    Se agrega DataGriedViewer con conexión asistida

---
+++ RestauranteBD/RestauranteBD/Asistido1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestauranteBD
+{
+    public partial class Asistido1 : Form
+    {
+        public Asistido1()
+        {
+            InitializeComponent();
+        }
+
+        private void Asistido1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'restauranteDataSet1.platos' table. You can move, or remove it, as needed.
+            this.platosTableAdapter.Fill(this.restauranteDataSet1.platos);
+
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.platosBindingSource.EndEdit();
+            this.platosTableAdapter.Update(this.restauranteDataSet1);
+        }
+    }
+}
--- RestauranteBD/RestauranteBD/Program.cs
+++ RestauranteBD/RestauranteBD/Program.cs
@@ -16,7 +16,7 @@ namespace RestauranteBD
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new Asistido1());
         }
     }
 }
